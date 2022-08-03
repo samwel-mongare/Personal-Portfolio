4c4c30eb8d8c3b0e590aa5e811e1801b571daf28
mong'are <mongaresams@gmail.com>
2022-08-03 14:32:09 +0300

    Move card data to a new js file and used loop to call them

--- details.js
+++ details.js
@@ -1,12 +1,6 @@
 const body = document.querySelector('body');
-const open = document.getElementById('open');
-const open2 = document.getElementById('open2');
-const open3 = document.getElementById('open3');
-const open4 = document.getElementById('open4');
-const open5 = document.getElementById('open5');
-const open6 = document.getElementById('open6');
-const open7 = document.getElementById('open7');
-const open8 = document.getElementById('open8');
+const open = document.querySelectorAll('.open')
+
 const section = document.createElement('section');
 const h3 = document.createElement('h3');
 const p = document.createElement('p');
@@ -103,91 +97,6 @@ img2.addEventListener('click', () => {
   section.classList.remove('show');
 });
 
-const cardsDetail1 = [
-  {
-    name: 'Bar in Motion',
-    description: 'The Bar in Motion is a project done in the first Microverse module milestone. I created the project as a reference to the halloween holiday function organized by the Bar in Motion Communuty. The project was able to communicate the fun events that were organized for the day and also worked as a marketing strategy for the company by displaying their previous events',
-    canopy: 'CANOPY',
-    back: 'Back End Dev',
-    year: '2021',
-    image: 'images/bar1.png',
-    tech1: 'html',
-    tech2: 'css',
-    tech3: 'javascript',
-    closBtn: 'images/close.png',
-    buttn1: 'See live',
-    buttn2: 'See Source',
-    counterOne: 'images/Counter.jpg',
-    counterTwo: 'images/Counter.jpg',
-    live: 'images/live.jpg',
-    gity: 'images/gitty.jpg',
-    liveLink: 'https://mosams.github.io/Module-One-Capstone-Project/',
-    liveGit: 'https://github.com/Mosams/Module-One-Capstone-Project.git'
-  },
-
-  {
-    name: 'Multi-Post Stories',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    canopy: 'CANOPY',
-    back: 'Back End Dev',
-    year: '2015',
-    image: 'images/card2.jpg',
-    tech1: 'html',
-    tech2: 'css',
-    tech3: 'javascript',
-    closBtn: 'images/close.jpg',
-    buttn1: 'See live',
-    buttn2: 'See Source',
-    counterOne: 'images/Counter.jpg',
-    counterTwo: 'images/Counter.jpg',
-    live: 'images/live.jpg',
-    gity: 'images/gitty.jpg',
-    liveLink: 'https://mosams.github.io/Module-One-Capstone-Project/',
-    liveGit: 'https://github.com/Mosams/Module-One-Capstone-Project.git'
-  },
-  {
-    name: 'Tonic',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    canopy: 'CANOPY',
-    back: 'Back End Dev',
-    year: '2015',
-    image: 'images/card3.jpg',
-    tech1: 'html',
-    tech2: 'css',
-    tech3: 'javascript',
-    closBtn: 'images/close.jpg',
-    buttn1: 'See live',
-    buttn2: 'See Source',
-    counterOne: 'images/Counter.jpg',
-    counterTwo: 'images/Counter.jpg',
-    live: 'images/live.jpg',
-    gity: 'images/gitty.jpg',
-    liveLink: 'https://mosams.github.io/Module-One-Capstone-Project/',
-    liveGit: 'https://github.com/Mosams/Module-One-Capstone-Project.git'
-  },
-  {
-    name: 'Multi-Post Stories',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    canopy: 'CANOPY',
-    back: 'Back End Dev',
-    year: '2015',
-    image: 'images/card4.jpg',
-    tech1: 'html',
-    tech2: 'css',
-    tech3: 'javascript',
-    closBtn: 'images/close.jpg',
-    buttn1: 'See live',
-    buttn2: 'See Source',
-    counterOne: 'images/Counter.jpg',
-    counterTwo: 'images/Counter.jpg',
-    live: 'images/live.jpg',
-    gity: 'images/gitty.jpg',
-    liveLink: 'https://mosams.github.io/Module-One-Capstone-Project/',
-    liveGit: 'https://github.com/Mosams/Module-One-Capstone-Project.git'
-  }
-];
-
-
 function displayGit() {
   for(let liveGity of cardsDetail1) {
     window.open(`${liveGity.liveGit}`);
@@ -208,35 +117,34 @@ button2.addEventListener('click', (e) => {
 });
 
 function displayInfo() {
+  open.forEach(element => {
+    element.addEventListener('click', (e) => {
+      cardHeading = e.target.parentElement.parentElement.firstChild.nextElementSibling.textContent;
 
-  for(let cardDetailsInfo of cardsDetail1) {
-    section.classList.add('show');
+      for(let i = 0; i < cardsDetail1.length; i++) {
+        if(cardHeading === cardsDetail1[i].name) {
+          section.classList.add('show');
 
-    document.getElementById('heading3').innerHTML = cardDetailsInfo.name;
-    document.getElementById('close-btn').src = cardDetailsInfo.closBtn;
-    document.getElementById('project-imgg').src = cardDetailsInfo.image;
-    document.getElementById('parag').innerHTML = cardDetailsInfo.description;
-    document.getElementById('cano').innerHTML = cardDetailsInfo.canopy;
-    document.getElementById('counter1').src = cardDetailsInfo.counterOne;
-    document.getElementById('backyy').innerHTML = cardDetailsInfo.back;
-    document.getElementById('counter2').src = cardDetailsInfo.counterOne;
-    document.getElementById('year').innerHTML = cardDetailsInfo.year;
-    document.getElementById('html').innerHTML = cardDetailsInfo.tech1;
-    document.getElementById('css').innerHTML = cardDetailsInfo.tech2;
-    document.getElementById('java').innerHTML = cardDetailsInfo.tech3;
-    document.getElementById('live-txt').innerHTML = cardDetailsInfo.buttn1;
-    document.getElementById('git-txt').innerHTML = cardDetailsInfo.buttn2;
-    document.getElementById('live').src = cardDetailsInfo.live;
-    document.getElementById('git').src = cardDetailsInfo.gity;
-  }
+          document.getElementById('heading3').innerHTML = cardsDetail1[i].name;
+          document.getElementById('close-btn').src = cardsDetail1[i].closBtn;
+          document.getElementById('project-imgg').src = cardsDetail1[i].image;
+          document.getElementById('parag').innerHTML = cardsDetail1[i].description;
+          document.getElementById('cano').innerHTML = cardsDetail1[i].canopy;
+          document.getElementById('counter1').src = cardsDetail1[i].counterOne;
+          document.getElementById('backyy').innerHTML = cardsDetail1[i].back;
+          document.getElementById('counter2').src = cardsDetail1[i].counterOne;
+          document.getElementById('year').innerHTML = cardsDetail1[i].year;
+          document.getElementById('html').innerHTML = cardsDetail1[i].tech1;
+          document.getElementById('css').innerHTML = cardsDetail1[i].tech2;
+          document.getElementById('java').innerHTML = cardsDetail1[i].tech3;
+          document.getElementById('live-txt').innerHTML = cardsDetail1[i].buttn1;
+          document.getElementById('git-txt').innerHTML = cardsDetail1[i].buttn2;
+          document.getElementById('live').src = cardsDetail1[i].live;
+          document.getElementById('git').src = cardsDetail1[i].gity;
+        }
+      };
+    });
+  });
+}
 
-  }
-
-open.addEventListener('click', displayInfo);
-open2.addEventListener('click', displayInfo);
-open3.addEventListener('click', displayInfo);
-open4.addEventListener('click', displayInfo);
-open5.addEventListener('click', displayInfo);
-open6.addEventListener('click', displayInfo);
-open7.addEventListener('click', displayInfo);
-open8.addEventListener('click', displayInfo);
+displayInfo()
